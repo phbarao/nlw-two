15f3747f67b69405e65622bf8384b179bb29c121
Pedro Barão <phbarao@gmail.com>
2020-10-27 22:55:07 -0300

    html and css ok + js start

---
+++ scripts/addField.js
@@ -0,0 +1,22 @@
+// Procurar o botao
+// Quando clicar no botao
+// Executar uma acao
+// Duplicar os campos
+
+document.querySelector("#add-time").addEventListener("click", cloneField);
+
+function cloneField() {
+  // Qual campo duplicar?
+  const newFieldContainer = document
+    .querySelector(".schedule-item")
+    .cloneNode(true);
+  // Pegar os campos:
+  const fields = newFieldContainer.querySelectorAll("input");
+
+  // Para cada campo, limpar
+  fields.forEach(function (field) {
+    field.value = "";
+  });
+  // Colocar na página: Onde?
+  document.querySelector("#schedule-items").appendChild(newFieldContainer);
+}
