48b32ee65f2ae51e3553834fdb46f6e304757855
Pedro Barão <phbarao@gmail.com>
2021-06-23 20:33:49 -0300

    adding data

--- src/database/test.js
+++ src/database/test.js
@@ -8,8 +8,7 @@ Database.then(async (db) => {
     avatar:
       'https://avatars2.githubusercontent.com/u/62365336?s=460&u=77f3d8aeebff51881efffaa2a4c0d48537538e09&v=4',
     whatsapp: '89972341232',
-    bio:
-      'Entusiasta das melhores tecnologias de produção musical. <br><br> Apaixonado por música',
+    bio: 'Entusiasta das melhores tecnologias de produção musical. <br><br> Apaixonado por música',
   };
 
   classValue = {
